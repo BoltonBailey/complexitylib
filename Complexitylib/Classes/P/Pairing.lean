97ec9142339bd45d28fedca3317c0ed41c1bb305
Bolton Bailey <bolton.bailey@gmail.com>
2026-08-29 23:15:57 -0700

    import reduce

--- Complexitylib/Classes/P/Pairing.lean
+++ Complexitylib/Classes/P/Pairing.lean
@@ -4,7 +4,8 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Bolton Bailey
 -/
 module
-public import Complexitylib.Classes.P.Cobham.Internal
+public import Complexitylib.Encoding.Pairing
+import Complexitylib.Classes.P.Cobham.Internal
 public import Complexitylib.Classes.P.Preimage
 
 /-!
